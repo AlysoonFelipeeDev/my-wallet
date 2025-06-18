5a4d3a98b8fdf0ccdce7b8ee82d160dff51f9fa5
jean <alysoonfelipe@hotmail.com>
2025-06-18 20:58:22 -0300

    corrige validação do email no schema de login com .email()

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -2,11 +2,11 @@ import Joi from "joi";
 
     export const registerSchema = Joi.object({
         name: Joi.string().required(),
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
         password: Joi.string().min(6).required()
     }); 
 
     export const logginSchema = Joi.object({
-        email: Joi.string().required(),
+        email: Joi.string().email().required(),
         password: Joi.string().required()
     })
